5aa471844b445edb30b5ec42104b02fe163e99fc
Gian Masto <gianmasto1@gmail.com>
2025-09-29 07:43:48 -0300

    Docker support and readme

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
         <AuthProvider>
           <NotificationProvider>
             {children}
-            <ConnectionStatus />
           </NotificationProvider>
         </AuthProvider>
         <Toaster position="top-right" />
